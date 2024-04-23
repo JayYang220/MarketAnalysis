19ab496665bd1b0e915ef8d28e7cbff166cfee86
JayYang220 <y123154@gmail.com>
2024-04-23 21:08:15 +0800

    1.2.0
    更新LSTM相關的函式

--- Main/API/stockInfo.py
+++ Main/API/stockInfo.py
@@ -3,117 +3,417 @@ import yfinance as yf
 # Guide https://algotrading101.com/learn/yahoo-finance-api-guide/
 import os
 import pandas as pd
-import pandas_datareader as pdr
-import pandas_datareader.data as web
-import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+# Import the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 class StockManager:
-    def __init__(self, absPath):
-        self.historyDataPath = os.path.join(absPath, "Data")
+    def __init__(self, abs_path):
+        self.history_data_path = os.path.join(abs_path, "Data")
 
         # 讀取庫存的CSV名稱 集中成list管理
-        self.stockNameList = self.loadStockList()
+        self.stock_name_list = self.__init_load_stock_list()
 
         # 建立stock class 集中成list管理
-        self.stockList = self.initStockClass()
+        self.stock_class_list = self.__init_create_stock_class()
 
-    def loadStockList(self):
-        if os.path.exists(self.historyDataPath) is False:
-            os.mkdir(self.historyDataPath)
+    def __init_load_stock_list(self):
+        """讀取庫存的CSV名稱 集中成list管理"""
+        if os.path.exists(self.history_data_path) is False:
+            os.mkdir(self.history_data_path)
             return []
         else:
-            fileList = os.listdir(self.historyDataPath)
+            fileList = os.listdir(self.history_data_path)
             fileListWithoutExtension = [os.path.splitext(file)[0] if file.endswith('.csv') else file for file in fileList]
-            self.stockNameList = fileListWithoutExtension
+            self.stock_name_list = fileListWithoutExtension
             return fileListWithoutExtension
 
-    def initStockClass(self):
+    def __init_create_stock_class(self):
+        """建立stock class 集中成list管理"""
         stockClassList = []
-        for stockName in self.stockNameList:
-            stockClassList.append(Stock(self.historyDataPath, stockName))
+        for stock_name in self.stock_name_list:
+            stockClassList.append(Stock(self.history_data_path, stock_name))
         return stockClassList
 
-    def createStockClass(self, stockName: str):
-        if stockName in self.stockNameList:
+    def create_stock_class(self, stock_name: str):
+        if stock_name in self.stock_name_list:
             print("This stock already exists in the list.")
             return
 
         # 嘗試downloadTicker，若名稱不存在時會拋出錯誤
         try:
-            stock = Stock(self.historyDataPath, stockName)
-            stock.downloadTicker()
-            stock.downloadHistoryData()
+            stock = Stock(self.history_data_path, stock_name)
+            stock.download_ticker()
+            stock.download_history_data()
         except Exception as e:
             print(e)
-            print("You can retrieve stock names from Yahoo Finance at https://finance.yahoo.com/")
+            print("You can retrieve stock names from Yahoo Finance. https://finance.yahoo.com/")
             return
 
-        self.stockList.append(stock)
-        self.stockNameList.append(stockName)
-        print(f"{stockName} Addition completed")
+        self.stock_class_list.append(stock)
+        self.stock_name_list.append(stock_name)
+        print(f"{stock_name} Addition completed")
 
 
-    def updateAll(self):
+    def update_all(self):
         """更新所有股票資訊"""
-        for stock in self.stockList:
-            stock.downloadHistoryData()
+        for stock in self.stock_class_list:
+            stock.download_history_data()
 
-    def showStockList(self):
+    def show_stock_list(self):
         """顯示庫存的CSV名稱"""
-        for index, stock in enumerate(self.stockNameList):
+        for index, stock in enumerate(self.stock_name_list):
             print(f"{index:>3d}. {stock}")
 
 class Stock:
-    def __init__(self, historyDataPath, stockName: str):
-        self.stockName = stockName
-        self.historyDataFile = os.path.join(historyDataPath, self.stockName + ".csv")
+    def __init__(self, historyDataPath, stock_name: str):
+        self.stock_name = stock_name
+        self.history_data_file = os.path.join(historyDataPath, self.stock_name + ".csv")
 
         # 初始化為None，待使用者輸入需求時再抓取
         self.ticker = None
-        self.companyInfo = None
-        self.historyData = None
+        self.company_info = None
+        self.history_data = None
 
-    def downloadTicker(self):
+    def download_ticker(self):
         """下載 ticker"""
-        self.ticker = yf.Ticker(self.stockName)
-        self.companyInfo = self.ticker.info
+        self.ticker = yf.Ticker(self.stock_name)
+        self.company_info = self.ticker.info
 
         # 股票名稱錯誤時，仍會返回一個dict，利用下列特徵確認股票名稱是否正確
         if 'previousClose' not in self.ticker.info:
             raise AssertionError("Stock name error.")
-        self.historyData = self.ticker.history(period='max', interval='1d')
+        self.history_data = self.ticker.history(period='max', interval='1d')
 
-    def showCompanyInfo(self):
+    def show_company_info(self):
         """顯示 CompanyInfo"""
         if self.ticker is None:
-            self.downloadTicker()
-        for key in self.companyInfo.keys():
-            print(f"{key:30s} {self.companyInfo[key]}")
+            self.download_ticker()
+        for key in self.company_info.keys():
+            print(f"{key:30s} {self.company_info[key]}")
 
-    def showHistoryData(self):
+    def show_history_data(self):
         """顯示 HistoryData"""
-        if self.historyData is None:
-            self.loadHistory()
-        print(self.historyData.head)
+        if self.history_data is None:
+            self.load_history()
+        print(self.history_data.head)
 
-    def loadHistory(self):
+    def load_history(self):
         """讀取 HistoryData"""
         try:
-            self.historyData = pd.read_csv(self.historyDataFile)
+            self.history_data = pd.read_csv(self.history_data_file)
         except Exception as e:
             print(e)
 
-    def downloadHistoryData(self, period: str = 'max', interval: str = '1d'):
+    def download_history_data(self, period: str = 'max', interval: str = '1d'):
         """Download HistoryData"""
         # 舊方法
+        # import pandas_datareader.data
         # yf.pdr_override()
         # start_date = dt.datetime(1900, 1, 10)
         # end_date = dt.datetime(2100, 3, 18)
-        # self.history = web.get_data_yahoo(self.stockName, start_date, end_date)
+        # self.history = pandas_datareader.data.get_data_yahoo(self.stockName, start_date, end_date)
 
-        self.downloadTicker()
-        self.historyData.to_csv(self.historyDataFile)
-        print(f"{self.stockName} Update completed")
+        self.download_ticker()
+        self.history_data.to_csv(self.history_data_file)
+        print(f"{self.stock_name} Update completed")
+
+    def LSTM_function(self):
+        """建立並繪圖"""
+        AnalysisStock(self.stock_name, self.history_data_file)
+
+    def test(self):
+        # https://ithelp.ithome.com.tw/articles/10206312?authuser=1
+        # 取消Alarm
+        # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+        datasetLen = 7306  # 設定訓練資料夾總數
+        datatrainPre = 1200  # 取最近n日做為訓練用資料
+        datasetReal = 20  # 設定最近n日的data作為真實資料
+        timeStep = 60  # 設定多少區間作為時間步長
+
+        try:
+            # 讀取訓練集
+            dataset_train = pd.read_csv(self.history_data_file)
+
+            # 讀取所有行+第2列
+            # .values是將DataFrame 轉換為 numpy array，也可以不用，因為後MinMaxScaler會自動轉換成numpy array再處理
+            # 必須使用[:,1:2]，若用[:,1]只會得到1維array
+            training_set = dataset_train.iloc[:, 1:2].values
+
+            # 設定縮放range
+            sc = MinMaxScaler(feature_range=(0, 1))
+            # 進行縮放，返回2維 numpy array
+            training_set_scaled = sc.fit_transform(training_set)
+
+            X_train = []  # 預測點的前 60 天的資料
+            y_train = []  # 預測點
+
+            offset = datasetLen - datatrainPre  # 訓練資料集起始點偏移
+            for i in range(timeStep + offset, datasetLen):
+                # 將60為一個區間，step1，逐漸將所有data加入X_train
+                X_train.append(training_set_scaled[i - timeStep:i, 0])
+                # 將60為一個區間，step1，逐漸將所有data加入y_train
+                y_train.append(training_set_scaled[i, 0])
+
+            # 轉成numpy array的格式，以利輸入 RNN
+            X_train, y_train = np.array(X_train), np.array(y_train)
+
+            # 將X_train重整為3維array
+            # np.reshape X_train.shape[0]=第1個維度大小 (樣本總數), X_train.shape[1]=第2個維度大小 (時間步長)
+            X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+            # Initialising the RNN
+            regressor = Sequential()
+
+            # Adding the first LSTM layer and some Dropout regularisation
+            # units是神經元數量, return_sequences=True則是為了傳遞到下一層
+            # Dropout是指訓練過程隨機將部分輸出設為0，以防止過擬合，增強泛化能力
+            regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+            regressor.add(Dropout(0.2))
+
+            # Adding a second LSTM layer and some Dropout regularisation
+            regressor.add(LSTM(units=50, return_sequences=True))
+            regressor.add(Dropout(0.2))
+
+            # Adding a third LSTM layer and some Dropout regularisation
+            regressor.add(LSTM(units=50, return_sequences=True))
+            regressor.add(Dropout(0.2))
+
+            # Adding a fourth LSTM layer and some Dropout regularisation
+            regressor.add(LSTM(units=50))
+            regressor.add(Dropout(0.2))
+
+            # Adding the output layer
+            # 輸出神經元，通常只有1個
+            regressor.add(Dense(units=1))
+
+            # Compiling
+            # optimizer優化器
+            # loss損失函數
+            regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+            # 進行訓練
+            # epochs是指對整個訓練的完整跌代，增加可提高訓練精度但也可能導致過擬合
+            # batch_size是指一次處理的樣本數量，批量大可加快訓練速度，批量小可加快收斂速度，但較不穩定
+            regressor.fit(X_train, y_train, epochs=100, batch_size=32)
+
+            # 製作真實資料
+            dataset_real = dataset_train.tail(datasetReal)
+            dataset_real = pd.concat([dataset_train.iloc[:1], dataset_real])
+            real_stock_price = dataset_real.iloc[:, 1:2].values
+
+            # 縱向合併
+            dataset_total = pd.concat((dataset_train['Open'], dataset_real['Open']), axis=0)
+
+            inputs = dataset_total[len(dataset_total) - len(dataset_real) - 60:].values
+            inputs = inputs.reshape(-1, 1)
+            inputs = sc.transform(inputs)  # Feature Scaling
+
+            X_test = []
+            for i in range(timeStep, timeStep + datasetReal):  # timeStep和之前一樣, timeStep+真實資料集
+                X_test.append(inputs[i - timeStep:i, 0])
+            X_test = np.array(X_test)
+            X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))  # Reshape 成 3-dimension
+
+            predicted_stock_price = regressor.predict(X_test)
+            predicted_stock_price = sc.inverse_transform(predicted_stock_price)  # to get the original scale
+
+            # Visualising the results
+            plt.plot(real_stock_price, color='red', label=f'Real {self.stock_name} Stock Price')  # 紅線表示真實股價
+            plt.plot(predicted_stock_price, color='blue', label=f'Predicted {self.stock_name} Stock Price')  # 藍線表示預測股價
+            plt.title(f'{self.stock_name} Stock Price Prediction')
+            plt.xlabel('Time')
+            plt.ylabel(f'{self.stock_name} Stock Price')
+            plt.legend()
+            plt.show()
+
+        except Exception as e:
+            raise e
+
+    def test2(self):
+        # https://daniel820710.medium.com/%E5%88%A9%E7%94%A8keras%E5%BB%BA%E6%A7%8Blstm%E6%A8%A1%E5%9E%8B-%E4%BB%A5stock-prediction-%E7%82%BA%E4%BE%8B-1-67456e0a0b
+        # Read
+        train = pd.read_csv(self.history_data_file)
+
+        # Augment Features
+        # 刪除時區
+        train["Date"] = train["Date"].str.replace(r'-\d{2}:\d{2}$', '', regex=True)
+        train["Date"] = pd.to_datetime(train["Date"])
+        train["year"] = train["Date"].dt.year
+        train["month"] = train["Date"].dt.month
+        train["date"] = train["Date"].dt.day
+        train["day"] = train["Date"].dt.dayofweek
+
+        # Normalization
+        train = train.drop(["Date"], axis=1)
+        train = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
+
+
+        # Build Training Data
+        pastDay = 30
+        futureDay = 5
+
+        X_train, Y_train = [], []
+        for i in range(train.shape[0] - futureDay - pastDay):
+            X_train.append(np.array(train.iloc[i:i + pastDay]))
+            Y_train.append(np.array(train.iloc[i + pastDay:i + pastDay + futureDay]["Close"]))
+
+        # 資料亂序
+        X, Y = np.array(X_train), np.array(Y_train)
+        np.random.seed(10)
+        randomList = np.arange(X.shape[0])
+        np.random.shuffle(randomList)
+        X, Y = X[randomList], Y[randomList]
+
+
+        # Training data & Validation data
+        rate = 0.1
+        X_train = X[int(X.shape[0] * rate):]
+        Y_train = Y[int(Y.shape[0] * rate):]
+        X_val = X[:int(X.shape[0] * rate)]
+        Y_val = Y[:int(Y.shape[0] * rate)]
+
+        # from 2 dimension to 3 dimension
+        Y_train = Y_train[:, np.newaxis]
+        Y_val = Y_val[:, np.newaxis]
+
+        model = self.test2_buildOneToOneModel(X_train.shape)
+        callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+        model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
+
+    def test2_buildOneToOneModel(self, shape):
+        model = Sequential()
+        model.add(LSTM(10, input_length=shape[1], input_dim=shape[2], return_sequences=True))
+        # output shape: (1, 1)
+        model.add(TimeDistributed(Dense(1)))  # or use model.add(Dense(1))
+        model.compile(loss="mse", optimizer="adam")
+        model.summary()
+        return model
+
+
+class AnalysisStock:
+    # https://ithelp.ithome.com.tw/articles/10206312?authuser=1
+    # 取消Alarm
+    # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    def __init__(self, stock_name, history_data_file):
+        self.stock_name = stock_name
+        self.history_data_file = history_data_file
+
+        self.dataset_len = self.__get_dataset_len()  # 取得訓練資料集，資料總數
+        self.data_train_pre_len = 1200  # 取最近n日做為訓練用資料
+        self.dataset_real_len = 20  # 設定最近n日的data作為真實資料
+        self.time_step_len = 60  # 設定多少區間作為時間步長
+
+        self.train_dataset = pd.read_csv(self.history_data_file)  # 讀取訓練集
+        self.sc = MinMaxScaler(feature_range=(0, 1))  # 設定縮放range
+
+        self.step1_prepare_data()
+        self.step2_training()
+        self.step3_output()
+
+    def __get_dataset_len(self):
+        """返回資料集資料總數"""
+        return len(pd.read_csv(self.history_data_file))
+
+    def step1_prepare_data(self):
+        # 讀取所有行+第2列
+        # .values是將DataFrame 轉換為 numpy array，也可以不用，因為後MinMaxScaler會自動轉換成numpy array再處理
+        # 必須使用[:,1:2]，若用[:,1]只會得到1維array
+        training_set = self.train_dataset.iloc[:, 1:2].values
+
+        # 進行縮放，返回2維 numpy array
+        training_set_scaled = self.sc.fit_transform(training_set)
+
+        x_train = []  # 預測點的前 60 天的資料
+        self.y_train = []  # 預測點
+
+        offset = self.dataset_len - self.data_train_pre_len  # 訓練資料集起始點偏移
+        for i in range(self.time_step_len + offset, self.dataset_len):
+            # 將60為一個區間，step1，逐漸將所有data加入X_train
+            x_train.append(training_set_scaled[i - self.time_step_len:i, 0])
+            # 將60為一個區間，step1，逐漸將所有data加入y_train
+            self.y_train.append(training_set_scaled[i, 0])
+
+        # 轉成numpy array的格式，以利輸入 RNN
+        self.x_train, self.y_train = np.array(x_train), np.array(self.y_train)
+
+        # 將X_train重整為3維array
+        # np.reshape X_train.shape[0]=第1個維度大小 (樣本總數), X_train.shape[1]=第2個維度大小 (時間步長)
+        self.x_train = np.reshape(self.x_train, (self.x_train.shape[0], self.x_train.shape[1], 1))
+
+    def step2_training(self):
+        # Initialising the RNN
+        self.regressor = Sequential()
+
+        # Adding the first LSTM layer and some Dropout regularisation
+        # units是神經元數量, return_sequences=True則是為了傳遞到下一層
+        # Dropout是指訓練過程隨機將部分輸出設為0，以防止過擬合，增強泛化能力
+        self.regressor.add(LSTM(units=50, return_sequences=True, input_shape=(self.x_train.shape[1], 1)))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding a second LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(units=50, return_sequences=True))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding a third LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(units=50, return_sequences=True))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding a fourth LSTM layer and some Dropout regularisation
+        self.regressor.add(LSTM(units=50))
+        self.regressor.add(Dropout(0.2))
+
+        # Adding the output layer
+        # 輸出神經元，通常只有1個
+        self.regressor.add(Dense(units=1))
+
+        # Compiling
+        # optimizer優化器
+        # loss損失函數
+        self.regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+        # 進行訓練
+        # epochs是指對整個訓練的完整跌代，增加可提高訓練精度但也可能導致過擬合
+        # batch_size是指一次處理的樣本數量，批量大可加快訓練速度，批量小可加快收斂速度，但較不穩定
+        self.regressor.fit(self.x_train, self.y_train, epochs=100, batch_size=32)
+
+
+    def step3_output(self):
+        # 製作真實資料
+        dataset_real = self.train_dataset.tail(self.dataset_real_len)    # 返回最後n行，作為真實資料
+        real_stock_price = dataset_real.iloc[:, 1:2].values
+
+        # 縱向合併
+        dataset_total = pd.concat((self.train_dataset['Open'], dataset_real['Open']), axis=0)
+
+        inputs = dataset_total[len(dataset_total) - len(dataset_real) - 60:].values
+        inputs = inputs.reshape(-1, 1)
+        inputs = self.sc.transform(inputs)  # Feature Scaling
+
+        X_test = []
+        for i in range(self.time_step_len, self.time_step_len + self.dataset_real_len):  # timeStep和之前一樣, timeStep+真實資料集
+            X_test.append(inputs[i - self.time_step_len:i, 0])
+        X_test = np.array(X_test)
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))  # Reshape 成 3-dimension
+
+        predicted_stock_price = self.regressor.predict(X_test)
+        predicted_stock_price = self.sc.inverse_transform(predicted_stock_price)  # to get the original scale
+
+        # Visualising the results
+        plt.plot(real_stock_price, color='red', label=f'Real {self.stock_name} Stock Price')  # 紅線表示真實股價
+        plt.plot(predicted_stock_price, color='blue', label=f'Predicted {self.stock_name} Stock Price')  # 藍線表示預測股價
+        plt.title(f'{self.stock_name} Stock Price Prediction')
+        plt.xlabel('Time')
+        plt.ylabel(f'{self.stock_name} Stock Price')
+        plt.legend()
+        plt.show()
+
 
 # 測試用
 if __name__ == "__main__":
--- Main/main.py
+++ Main/main.py
@@ -1,42 +1,64 @@
 import os
 from API import StockManager
 
+debug = False
+__version__ = "1.2.0"
+
 def main():
     # Get the absolute path and create a StockManager to manage downloaded historical data
-    manager = StockManager(absPath=os.path.abspath("../"))
+    manager = StockManager(abs_path=os.path.abspath("../"))
 
     while True:
         while True:
             print('Downloaded data:')
-            manager.showStockList()
-            ans = input('Select a stock from the list or choose another option. (A=Add New Stock, U=Update all History)\n')
+            manager.show_stock_list()
+            if debug:
+                ans = '0'
+            else:
+                ans = input('Select a stock from the list or choose another option. (A=Add New Stock, U=Update all History)\n')
+
             try:
                 ans = int(ans)
-                if 0 <= ans < len(manager.stockList):
+                if 0 <= ans < len(manager.stock_class_list):
                     index = ans
                     break
                 else:
                     print("Input error")
-            except:
+
+            except ValueError:
                 if ans.lower() == "a":
-                    stockName = input("Please enter the stock name (Ex:2330.TW).\n")
-                    manager.createStockClass(stockName)
+                    stock_name = input("Please enter the stock name (Ex:2330.TW).\n")
+                    manager.create_stock_class(stock_name)
                 elif ans.lower() == "u":
-                    manager.updateAll()
+                    manager.update_all()
                 else:
                     print("Input error")
 
+            except Exception as e:
+                print(f"Unknow Error: {e}")
+
         while True:
-            ans = input("Select an action. (I=Show Company Info, H=Show History Data, U=Update History Data, 0=Return)\n")
+            if debug:
+                ans = 'a'
+            else:
+                ans = input("Select an action. (I=Show Company Info, H=Show History Data, U=Update History Data, L=LTSM Function, 0=Return)\n")
             if ans.lower() == "i":
-                manager.stockList[index].showCompanyInfo()
+                # Show Company Info
+                manager.stock_class_list[index].show_company_info()
             elif ans.lower() == "h":
-                manager.stockList[index].showHistoryData()
+                # Show History Data
+                manager.stock_class_list[index].show_history_data()
             elif ans.lower() == "u":
-                manager.stockList[index].downloadHistoryData()
+                # Update History Data
+                manager.stock_class_list[index].download_history_data()
+            elif ans.lower() == "l":
+                # LTSM Function
+                manager.stock_class_list[index].LSTM_function()
             elif ans == "0":
                 break
+            if debug:
+                os.system('pause')
 
-
-main()
-os.system('pause')
+if __name__ == "__main__":
+    main()
+    os.system('pause')
